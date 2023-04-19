53f3465e3e398b8e5aaefbb5abf7259377b62739
ReniYo <122678511+ReniYo@users.noreply.github.com>
2023-04-19 14:17:02 +0300

    update vite.config.js
    file updated so watchlist.html to be visible on Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import {defineConfig} from 'vite'
 
 
 export default defineConfig({
-	plugins: []	
+	build: {
+    		rollupOptions: {
+      			input: {
+        		main: resolve(MovieWatchlist, 'index.html'),
+        		main: resolve(MovieWatchlist, 'watchlist.html'),
+      },
+    },
+  },	
 })
